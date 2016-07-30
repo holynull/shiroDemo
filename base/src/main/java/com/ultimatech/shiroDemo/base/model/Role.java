5a3dedeb90bcc7a2170f40e775dda06f9a4200f3
holynull <holynull@126.com>
2016-07-30 16:30:17 +0800

    把filter安全过滤数据,即url对应的访问权限数据存储到数据库中.

--- base/src/main/java/com/ultimatech/shiroDemo/base/model/Role.java
+++ base/src/main/java/com/ultimatech/shiroDemo/base/model/Role.java
@@ -8,49 +8,58 @@ import java.util.List;
  * Created by 张乐平 on 7/28 0028.
  */
 @Entity
-@Table(name="t_role")
+@Table(name = "t_role")
 public class Role {
 
     private Integer id;
     private String rolename;
-    private List<Permission> permissionList;//一个角色对应多个权限
+    private List<MyPermission> permissionList;//一个角色对应多个权限
     private List<User> userList;//一个角色对应多个用户
 
     @Id
-    @GeneratedValue(strategy= GenerationType.IDENTITY)
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     public Integer getId() {
         return id;
     }
+
     public void setId(Integer id) {
         this.id = id;
     }
+
     public String getRolename() {
         return rolename;
     }
+
     public void setRolename(String rolename) {
         this.rolename = rolename;
     }
-    @OneToMany(mappedBy="role",fetch = FetchType.LAZY)
-    public List<Permission> getPermissionList() {
+
+    @OneToMany(mappedBy = "role", fetch = FetchType.LAZY)
+    public List<MyPermission> getPermissionList() {
         return permissionList;
     }
-    public void setPermissionList(List<Permission> permissionList) {
+
+    public void setPermissionList(List<MyPermission> permissionList) {
         this.permissionList = permissionList;
     }
+
     @ManyToMany(fetch = FetchType.LAZY)
-    @JoinTable(name="t_user_role",joinColumns={@JoinColumn(name="role_id")},inverseJoinColumns={@JoinColumn(name="user_id")})
+    @JoinTable(name = "t_user_role", joinColumns = {@JoinColumn(name = "role_id")},
+            inverseJoinColumns = {@JoinColumn(name = "user_id")})
     public List<User> getUserList() {
         return userList;
     }
+
     public void setUserList(List<User> userList) {
         this.userList = userList;
     }
 
     @Transient
-    public List<String> getPermissionsName(){
-        List<String> list=new ArrayList<String>();
-        for (Permission per : this.permissionList) {
-            list.add(per.getPermissionname());
+    public List<String> getPermissionsString() {
+        List<String> list = new ArrayList<String>();
+        for (MyPermission per : this.permissionList) {
+            list.add(per.getDataType() + ":" + per.getOperation() + (per.getDataDomain() == null || per.getDataDomain()
+                    .equals("") ? "" : (":" + per.getDataDomain())));
         }
         return list;
     }
